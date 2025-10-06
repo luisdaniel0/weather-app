0d95d2fd320bb6e02851afad3696171f48e97ffb
Luis Sanchez <luisanchezdh@gmail.com>
2025-10-06 00:34:40 -0400

    added assets folder with icons and dynamically accessed them and all of the current weather information is being dynamically displayed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const searchBtn = document.getElementById('searchBtn');
 const form = document.querySelector('#form');
 const input = form.elements.searchBtn; // Change this to match your input's name attribute
-console.log(input);
+
+//header
+const currentCity = document.querySelector('.currentCity');
+const currentCityName = document.querySelector('.currentCityName');
+const currentCityDate = document.querySelector('.currentCityDate');
+const headerCityTemp = document.querySelector('.headerCityTemp');
+const headerCityFeelsLike = document.querySelector('.headerCityFeelsLike');
+const headerCityConditions = document.querySelector('.headerCityConditions');
+
+//current details container
+const wind = document.querySelector('.currentWind');
+const visibility = document.querySelector('.currentVisibility');
+const humidity = document.querySelector('.currentHumidity');
+const currentWeatherIcon = document.querySelector('.weather-icon');
+console.log(currentWeatherIcon);
+
+const iconMap = {};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('input works', input.value); // Use input.value instead of e.target.value
+  // Use input.value instead of e.target.value
   const userInput = input.value;
   //call the api with the user's input
   apiCall(userInput);
@@ -17,21 +33,32 @@ const apiCall = async (location) => {
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
+
+  //HEADER CONTAINER
   const cityTemp = data.currentConditions.temp;
-  console.log(cityTemp);
+  headerCityTemp.textContent = cityTemp;
   const cityDate = data.days[0].datetime;
-  console.log(cityDate);
+  currentCityDate.textContent = cityDate;
   const cityName = data.resolvedAddress;
-  console.log(cityName);
+  currentCityName.textContent = cityName;
   const currentConditions = data.currentConditions.conditions;
   console.log(currentConditions);
+  headerCityConditions.textContent = currentConditions;
+  const currentFeelsLike = data.currentConditions.feelslike;
+  headerCityFeelsLike.textContent = currentFeelsLike;
+
+  //CURRENT DETAILS CONTAINER
   const currentHumidity = data.currentConditions.humidity;
   console.log(currentHumidity);
+  humidity.textContent = currentHumidity;
   const currentVisibility = data.currentConditions.visibility;
   console.log(currentVisibility);
+  visibility.textContent = currentVisibility;
   const currentWind = data.currentConditions.windspeed;
   console.log(currentWind);
+  wind.textContent = currentWind;
   const currentIcon = data.currentConditions.icon;
+  currentWeatherIcon.src = require(`./assets/${currentIcon}.svg`);
   console.log(currentIcon);
 
   const daysAfter = data.days;
